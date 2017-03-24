fbaa90c99f806c636b62f235932005b1c4d55343
DESKTOP-GTFM6U5\Spas Velinov <spasvelinov@gmail.com>
2017-03-24 15:17:49 +0100

    Initial commit

---
+++ AarhusWebDevCoop/Controller/ContactFormSurfaceController.cs
@@ -0,0 +1,54 @@
+﻿using AarhusWebDevCoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+
+
+namespace AarhusWebDevCoop.Controller
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index() 
+        {
+           
+            return PartialView("ContactForm", new ContactFormViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+           
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+               // success for sending email
+                TempData["success"] = true;
+                // create message
+                IContent emailMessage = Services.ContentService.CreateContent(model.Subject,CurrentPage.Id,"Message");
+
+                // set value to message properties
+                emailMessage.SetValue("senderName", model.Name);
+                emailMessage.SetValue("email", model.Email);
+                emailMessage.SetValue("subject", model.Subject);
+                emailMessage.SetValue("message", model.Message);
+
+                //Save
+                Services.ContentService.Save(emailMessage);
+ 
+            }
+            return RedirectToCurrentUmbracoPage();
+        }
+
+
+    }
+}
---
+++ AarhusWebDevCoop/ViewModels/ContactFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactFormViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "This is not a valid message!")]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
